89fb1f376e9f5a4b6690893a0ddf3125b2c101a7
nishikalkote <72296418+nishikalkote@users.noreply.github.com>
2020-10-04 01:58:06 +1000

    Update and rename helloWorld.py to Helloarray.py

---
+++ programs/Helloarray.py
@@ -0,0 +1,12 @@
+#LANGUAGE: Python
+# AUTHOR: Nishigandha Kalkote
+# GITHUB: "https://github.com/nishikalkote"
+
+
+print("HELLO WORLD!")
+arr = []
+n = int(input('Enter how many elements you want: '))
+for i in range(0, n):
+    x = input('Enter the numbers into the array: ')
+    arr.append(x)
+print(arr)
